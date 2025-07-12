86b5a263fc2f560d4accbfc0be8e4eee5165cef0
bioinsilico <jsegurmor@gmail.com>
2025-07-12 08:59:05 -0700

    embedding dataset cleanup

--- src/analysis/embedding_dataset.py
+++ src/analysis/embedding_dataset.py
@@ -18,32 +18,12 @@ class EmbeddingDataset:
         self.embeddings_classes = {}
         self.n_classes = {}
         self.embedding_path = embedding_path
-        self.embedding_classe_file = embedding_class_file
+        self.embedding_class_file = embedding_class_file
         self.depth = depth
         self.get_tp = get_tp(self.depth)
         self.load_embedding()
         self.load_class_number()
-        self.load_embedding_pairs()
         super().__init__()
-
-    def load_embedding_int8(self):
-        min_val = 1000
-        max_val = 0
-        for file in os.listdir(self.embedding_path):
-            embedding_id = ".".join(file.split(".")[0:-1])
-            v = np.array(list(pd.read_csv(f"{self.embedding_path}/{file}", header=None).iloc[:, 0].values))
-            if v.min() < min_val:
-                min_val = v.min()
-            if v.max() > max_val:
-                max_val = v.max()
-            self.embeddings[embedding_id] = v
-        for embedding_id in self.embeddings:
-            v = self.embeddings[embedding_id]
-            v = np.round((v - min_val) * 255.0 / (max_val - min_val)).astype(np.uint8)
-            norm = np.linalg.norm(v)
-            if norm > 0:
-                v = v / norm
-            self.embeddings[embedding_id] = v
 
     def load_embedding(self):
         for file in os.listdir(self.embedding_path):
@@ -55,7 +35,7 @@ class EmbeddingDataset:
             self.embeddings[embedding_id] = v
 
     def load_class_number(self):
-        scop_classes = [(row.strip().split("\t")[0], row.strip().split("\t")[1]) for row in open(self.embedding_classe_file)]
+        scop_classes = [(row.strip().split("\t")[0], row.strip().split("\t")[1]) for row in open(self.embedding_class_file)]
         for e_i, d_i in scop_classes:
             self.embeddings_classes[e_i] = d_i
             self.n_classes[e_i] = 0
@@ -64,33 +44,6 @@ class EmbeddingDataset:
                     continue
                 if is_tp(self.depth, d_i, d_j):
                     self.n_classes[e_i] += 1
-
-    def load_embedding_pairs(self):
-        ids = list(self.embeddings.keys())
-        n_pos = 0
-        n_neg = 0
-        while len(ids) > 0:
-            embedding_i = ids.pop()
-            for embedding_j in ids:
-                pred = 1 if self.embeddings_classes[embedding_i] == self.embeddings_classes[embedding_j] else 0
-                if pred == 1:
-                    n_pos += 1
-                else:
-                    n_neg += 1
-                self.embedding_pairs.append([
-                    embedding_i,
-                    embedding_j,
-                    pred
-                ])
-        print(f"Number of positives: {n_pos}, negatives: {n_neg}")
-
-    def pairs(self):
-        for embedding_pair in self.embedding_pairs:
-            yield (
-                self.embeddings[embedding_pair[0]],
-                self.embeddings[embedding_pair[1]],
-                embedding_pair[2]
-            )
 
     def domains(self, n=1):
         for embedding_id in [
